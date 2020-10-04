42d223854defa5a381421acb2bff0b9bd7f01545
repl.it user <replituser@example.com>
2020-10-04 06:53:01 +0000

    Added code to display an ASCII robot with dynamic eyes

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,12 @@
+eyeChar = input("Please enter a character for the eye")
+print("Beep's expression is now as follows:")
+
+print("\t##########")
+print("\t# ", eyeChar,"",eyeChar," #")
+print("\t#  ____  #")
+print("\t##########")
+print(" ################")
+print(" #  ##########  #")
+print(" #  ##########  #")
+print("    ##########")
+print("     ########")
